1eae45b7b766c971574f3fcb8dbeecf45929bd2b
KaripeNeerajKumar9511 <karipeneerajkumar2@gmail.com>
2026-08-24 15:42:39 +0530

    adding SEO tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import {
   SITE_DESCRIPTION,
   SITE_ICON,
@@ -54,7 +55,21 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6EGGF1QSM3"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6EGGF1QSM3');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
